686e82da951fc79b52af8184b71a9c68d079ded0
Mike van der Wal <MikeJordy1999@gmail.com>
2019-01-19 14:41:23 +0100

    Added a pause and resume button to the Simulation aswell as a view keeping track of the time and date.

--- src/nl/hanze/experience/parkinggarage/views/SimulationInfoView.java
+++ src/nl/hanze/experience/parkinggarage/views/SimulationInfoView.java
@@ -2,6 +2,8 @@ package nl.hanze.experience.parkinggarage.views;
 
 import nl.hanze.experience.mvc.*;
 import nl.hanze.experience.parkinggarage.models.SimulationInfoModel;
+import static nl.hanze.experience.parkinggarage.controllers.SimulationInfoController.*;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,12 +12,24 @@ import java.awt.*;
 public class SimulationInfoView extends View {
 
     private JLabel tickCountLabel;
+    private JLabel dateTimeLabel;
 
     public SimulationInfoView() {
         this.setLayout(new FlowLayout());
         add(new JLabel("Amount of ticks: "));
         tickCountLabel = new JLabel();
         this.add(tickCountLabel);
+        add(new JLabel("Time: "));
+        dateTimeLabel = new JLabel();
+        this.add(dateTimeLabel);
+
+        JButton pauseButton = new JButton("Pause");
+        this.add(pauseButton);
+        pauseButton.addActionListener(e ->  notifyController(EVENT_ID_PAUSE));
+
+        JButton resumeButton = new JButton("Resume");
+        this.add(resumeButton);
+        resumeButton.addActionListener(e ->  notifyController(EVENT_ID_RESUME));
     }
 
     @Override
@@ -25,8 +39,9 @@ public class SimulationInfoView extends View {
             throw new IllegalArgumentException("Wrong model given to view.update");
         }
         SimulationInfoModel simulationInfoModel = (SimulationInfoModel) model;
-        tickCountLabel.setBackground(Color.getHSBColor(100f, 100f, 100f));
+        //tickCountLabel.setBackground(Color.getHSBColor(100f, 100f, 100f));
         tickCountLabel.setText(Integer.toString((simulationInfoModel).getTickCount()));
+        dateTimeLabel.setText(simulationInfoModel.getFormattedTime());
     }
 
 }
