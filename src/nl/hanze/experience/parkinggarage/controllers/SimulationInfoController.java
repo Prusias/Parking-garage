d98127722fda935bfb8eb2571e5864773175e032
Mike van der Wal <MikeJordy1999@gmail.com>
2019-01-19 14:41:23 +0100

    Added a pause and resume button to the Simulation aswell as a view keeping track of the time and date.

--- src/nl/hanze/experience/parkinggarage/controllers/SimulationInfoController.java
+++ src/nl/hanze/experience/parkinggarage/controllers/SimulationInfoController.java
@@ -7,10 +7,22 @@ import nl.hanze.experience.parkinggarage.views.SimulationInfoView;
 
 public class SimulationInfoController extends Controller {
 
+    public static final int EVENT_ID_PAUSE = 1;
+    public static final int EVENT_ID_RESUME = 2;
+
     public SimulationInfoController(SimulationInfoModel model) {super(model); }
 
     @Override
     protected boolean event(View view, int event_id) {
+        if (event_id == EVENT_ID_PAUSE) {
+            SimulationInfoModel simulationInfoModel = (SimulationInfoModel) model;
+            simulationInfoModel.pauseSimulation();
+            return true;
+        } else if (event_id == EVENT_ID_RESUME) {
+            SimulationInfoModel simulationInfoModel = (SimulationInfoModel) model;
+            simulationInfoModel.resumeSimulation();
+            return true;
+        }
         return false;
     }
 
