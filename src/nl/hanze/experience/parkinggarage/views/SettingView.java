83d2364171e1fec3ae8a0351f9efd94353286688
Mike van der Wal <MikeJordy1999@gmail.com>
2019-01-25 18:25:47 +0100

    Some work on the settings menu

--- src/nl/hanze/experience/parkinggarage/views/SettingView.java
+++ src/nl/hanze/experience/parkinggarage/views/SettingView.java
@@ -6,7 +6,10 @@ import nl.hanze.experience.mvc.*;
 import nl.hanze.experience.parkinggarage.models.SettingModel;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+
+import static javax.swing.GroupLayout.Alignment;
 
 /**
  * @author Mike van der Velde
@@ -16,7 +19,9 @@ import java.awt.*;
 public class SettingView extends JFrameView {
 
     private Container container;
-    private JPanel jPanel;
+    private JPanel simulationSettings;
+    private JPanel buttons;
+    private GridLayout gridLayout;
     private JTextField amountOfFloorsInput;
     private JTextField amountOfRowsInput;
     private JTextField amountOfSpotsInput;
@@ -25,22 +30,37 @@ public class SettingView extends JFrameView {
     public SettingView() {
         this.setTitle("Settings");
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setSize(600, 600);
+        this.setResizable(true);
 
         container = this.getContentPane();
         container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 
-        jPanel = new JPanel();
-        container.add(jPanel);
+        gridLayout = new GridLayout(0, 2);
+        gridLayout.setHgap(10);
+        gridLayout.setVgap(10);
+
+        simulationSettings = new JPanel();
+        simulationSettings.setLayout(gridLayout);
+        simulationSettings.setSize(600, 160);
+        simulationSettings.setBorder(new EmptyBorder(10, 10, 10, 10));
+        container.add(simulationSettings);
+
+        container.add(new JSeparator());
+
+        buttons = new JPanel();
+        buttons.setLayout(new FlowLayout(FlowLayout.LEFT));
+        container.add(buttons);
 
         addFields();
 
         JButton saveButton = new JButton("Save");
-        jPanel.add(saveButton);
+        buttons.add(saveButton);
 
         saveButton.addActionListener(e -> notifyController(EventId.SAVE));
 
         this.pack();
-        this.setSize(400, 600);
+
     }
 
     @Override
@@ -62,21 +82,26 @@ public class SettingView extends JFrameView {
     }
 
     private void addFields() {
-        jPanel.add(new JLabel("Amount of floors the garage has."));
+        simulationSettings.add(new JLabel("Amount of floors the garage has."));
         amountOfFloorsInput = new JTextField(30);
-        jPanel.add(amountOfFloorsInput);
+        simulationSettings.add(amountOfFloorsInput);
 
-        jPanel.add(new JLabel("Amount of rows each floor has."));
+        simulationSettings.add(new JLabel("Amount of rows each floor has."));
         amountOfRowsInput = new JTextField(30);
-        jPanel.add(amountOfRowsInput);
+        simulationSettings.add(amountOfRowsInput);
 
-        jPanel.add(new JLabel("Amount of parking spots each row has."));
+        simulationSettings.add(new JLabel("Amount of parking spots each row has."));
         amountOfSpotsInput = new JTextField(30);
-        jPanel.add(amountOfSpotsInput);
+        simulationSettings.add(amountOfSpotsInput);
 
-        jPanel.add(new JLabel("The price of parking per 10 minutes in €."));
+        simulationSettings.add(new JLabel("The price of parking per 10 minutes in €."));
         priceInEuroInput = new JTextField(30);
-        jPanel.add(priceInEuroInput);
+        simulationSettings.add(priceInEuroInput);
     }
+
+    public String getAmountOfFloors() {return amountOfFloorsInput.getText();}
+    public String getamountOfRows() {return amountOfRowsInput.getText();}
+    public String getamountOfSpots() {return amountOfSpotsInput.getText();}
+    public String getpriceInEuro() {return priceInEuroInput.getText();}
 
 }
